5c82635690b413aefbad0c5857f0baee81d44882
Karim Sonbul <sonbul.karim@gmail.com>
2022-10-31 00:14:45 +0200

    Implemented Crossover function

--- src/KnapSack.java
+++ src/KnapSack.java
@@ -5,6 +5,10 @@ class Chromosome{
 	public int[] genes;
 	public int fitness;
 	
+	public void generateEmpty(int numberOfItems) {
+		this.genes = new int[numberOfItems];
+	}
+	
 	public void generate(int numberOfItems) {
 		this.genes = new int[numberOfItems];
 		for (int i = 0; i < numberOfItems; i++) {
@@ -15,18 +19,20 @@ class Chromosome{
 
         }
 	}
+	
 	public void printGenes() {
 		for(int i=0;i<genes.length;i++) {
 			System.out.print(genes[i]+" ");
 		}
-		System.out.println("\n");
+		System.out.println("fitness= "+this.fitness +"\n");
 	}
 }
 public class KnapSack {
     static int [] weights = {20,30,10,2,1};
-    static int [] values;
+    static int [] values = {10,5,2,7,1};
     static int NumberOfItems = 5;
     static int knapWeight = 40;
+    static double crossoverProbability = 0.6;
     final static int NumberOfPopulation = 5;
 
     public static ArrayList<Chromosome> generatePopulation() {
@@ -50,6 +56,15 @@ public class KnapSack {
        }
        return population;
     }
+    
+    public static void calculateSingleFitness(Chromosome chrom) {
+    	chrom.fitness=0;
+    	for (int j =0 ; j<NumberOfItems ; j++)
+        {
+            if(chrom.genes[j] == 1)
+                chrom.fitness+=values[j];
+        }
+    }
 
     public static void calculateFitnessValue(ArrayList<Chromosome> population) {
         for(int i=0 ; i<NumberOfPopulation ; i++)
@@ -65,17 +80,18 @@ public class KnapSack {
     }
 
     public static Chromosome[] chromosomeSelection(ArrayList<Chromosome> population) {
-        int totalFitnessValues = 0;
-        for(int i=0 ; i<NumberOfPopulation ; i++)
+        double totalFitnessValues = 0.0;
+        for(int i=0 ; i<NumberOfPopulation ; i++) {
             totalFitnessValues+=population.get(i).fitness;
+        }
+        	
 
-        float [] selectionProbability = new float[NumberOfPopulation];
+        double [] selectionProbability = new double[NumberOfPopulation];
 
         for(int i =0 ; i<NumberOfPopulation ; i++)
         {
             if(i==0)
-                selectionProbability[i] = population.get(i).fitness/totalFitnessValues;
-
+                selectionProbability[i] = (population.get(i).fitness)/totalFitnessValues;
             else
                 selectionProbability[i] = selectionProbability[i-1] + (population.get(i).fitness/totalFitnessValues);
         }
@@ -96,15 +112,54 @@ public class KnapSack {
 
         return selectedChromosomes;
     }
+    public static Chromosome crossover(Chromosome[] selected) {
+    	Chromosome offspring = new Chromosome();
+    	offspring.generateEmpty(NumberOfItems);
+    	float prob = new Random().nextFloat();
+    	System.out.println("Probability of crossover for current parents= " + prob + "\n");
+    	if(prob < crossoverProbability) {
+	    	int crossoverPoint = new Random().nextInt(1,NumberOfItems-1);
+	    	System.out.println("Crossover point= " + crossoverPoint + "\n");
+	    	int size = selected[0].genes.length;
+	    	for(int i=0;i<size;i++) {
+	    		if(i < crossoverPoint) {
+	    			offspring.genes[i] = selected[0].genes[i];
+	    		}
+	    		else {
+	    			offspring.genes[i] = selected[1].genes[i];
+	    		}
+	    	}
+	    	calculateSingleFitness(offspring);
+	    	return offspring;
+    	}
+    	return null;
+    }
+    public static void runAlgorithm() {
+    	ArrayList<Chromosome> chromosomes = new ArrayList<>();
+    	chromosomes = generatePopulation();
+    	calculateFitnessValue(chromosomes);
+    	Chromosome[] parents = new Chromosome[2];
+    	parents = chromosomeSelection(chromosomes);
+    	System.out.println("Population:");
+    	for(int i=0 ;i < NumberOfPopulation ; i++)
+        {
+            chromosomes.get(i).printGenes();
+        }
+    	System.out.println("Parents:");
+    	for(int i=0 ;i < 2 ; i++)
+        {
+            parents[i].printGenes();
+        }
+    	Chromosome offspring = new Chromosome();
+    	offspring = crossover(parents);
+    	System.out.println("Offspring:");
+    	offspring.printGenes();
+    }
 
 
 
     public static void main(String[] args) {
-        ArrayList<Chromosome> pop = generatePopulation();
-        for(int i=0 ;i < NumberOfItems ; i++)
-        {
-            pop.get(i).printGenes();
-        }
+    	runAlgorithm();
 
     }
 }
